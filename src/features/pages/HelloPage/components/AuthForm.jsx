408c6570cc3312a25230aee94d85dab90b770947
Vasyl <luchkaviktoria@gmail.com>
2026-03-01 20:49:08 +0200

    login form rename, added inputs

---
+++ src/features/pages/HelloPage/components/AuthForm.jsx
@@ -0,0 +1,11 @@
+const AuthForm = () => {
+    
+    return(
+        <>
+        Hello Wrld
+        <input about="Ім'я"/>
+        <input about="Пароль"/>
+        <input about="Пошта"/>
+        </>
+    )
+}
